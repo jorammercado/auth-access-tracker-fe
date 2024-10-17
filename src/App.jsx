290572706dbce819e6fe4820020a7a1f6f401cc8
arim83 <jorammercado@pursuit.org>
2024-10-17 16:16:47 -0400

    setup public/protected routes with corresponding element in main app

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,89 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
-
-
+  const [currentUser, setCurrentUser] = useState(null)
   return (
-    <>
+    <div className='app'>
+      <Router>
+        <div className="nav">
+          <NavBar currentUser={currentUser}
+            setCurrentUser={setCurrentUser} />
+        </div>
+        <main>
+          <Routes>
+            <Route path="/login"
+              element={
+                <PublicRoute
+                  element={Login}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
 
-      <h1>Red Canary Take Home - User Authentication and Access Tracking System Frontend</h1>
+            <Route path="/"
+              element={
+                <Navigate to="/login" />
+              }
+            />
 
-    </>
+            <Route path="/signup"
+              element={
+                <PublicRoute
+                  element={SignUp}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
+
+            <Route
+              path="/users/:user_id/profile/edit"
+              element={
+                <ProtectedRoute
+                  element={UserInfoEditForm}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
+
+            <Route
+              path="/users/:user_id/profile"
+              element={
+                <ProtectedRoute
+                  element={UserPortal}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
+
+            <Route
+              path="/home"
+              element={
+                <ProtectedRoute
+                  element={Home}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
+
+            <Route
+              path="*"
+              element={
+                <PublicRoute
+                  element={FourOFour}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
+          </Routes>
+        </main>
+      </Router>
+    </div>
   )
 }
 
