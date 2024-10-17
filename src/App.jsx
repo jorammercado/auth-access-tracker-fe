9e38f9faa8538186f59bc0e8c9380265cf9a1581
arim83 <jorammercado@pursuit.org>
2024-10-17 18:28:57 -0400

    import pages/components to App.jsx, fill those with boilerplate code for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import Home from "./pages/Home"
+import UserPortal from "./pages/UserPortal"
+import ProtectedRoute from "./components/ProtectedRoute"
+import UserInfoEditForm from "./components/UserInfoEditForm"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import PublicRoute from "./components/PublicRoute"
+import FourOFour from "./pages/FourOFour"
+import NavBar from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
---
+++ src/components/UserInfoEditForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserInfoEditForm = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserInfoEditForm
