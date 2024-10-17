7f78ca1f0faf355028fc12b9bb600b1253cc8dbb
arim83 <jorammercado@pursuit.org>
2024-10-17 19:43:44 -0400

    basic navagation bar setup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+import logo from "../assets/red-canary-logo.png"
 
-const NavBar = () => {
+export default function NavBar() {
     return (
-        <div>
-            
+        <div className="navbar" >
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                <div className="container-fluid">
+                    <div className="main-links">
+                        <Link to="/users/:user_id/profile">
+                            <p >User</p>
+                        </Link>
+                        <Link to="/signup">
+                            <p >Sign Up</p>
+                        </Link>
+                    </div>
+                    <span className="navbar-text">
+                        <p>User Authentication and Access Tracking System - Test </p>
+                    </span>
+                    <span className="image" >
+                        <Link to="/">
+                            <img src={logo} alt="Logo" height="55" />
+                        </Link>
+                    </span>
+                </div>
+            </nav>
         </div>
     )
 }
 
-export default NavBar
